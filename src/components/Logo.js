184c74c4813034a899365f3a88225ffd8cc9574f
Sai Rithwik M <rithwiksaim@gmail.com>
2023-02-17 18:07:02 +0530

    Added components like Login, DarkMode etc.

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import { Box, Heading } from "@chakra-ui/layout"
+import { useColorMode, Link } from "@chakra-ui/react";
+import { primaryTextColor } from '../styles/darkMode';
+
+const Logo = () => {
+    const { colorMode } = useColorMode()
+    return (
+        <Box p={8} pb={4}>
+            <Link href="/">
+                <Heading color={primaryTextColor[colorMode]}>📜 LastWord</Heading>
+            </Link>
+        </Box>
+    )
+};
+
+export default Logo;
