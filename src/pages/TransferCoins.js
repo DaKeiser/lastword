a7c11407264a601e7ae4455c6510e4308c978c3f
Sai Rithwik M <rithwiksaim@gmail.com>
2023-02-18 00:04:20 +0530

    Coin transfer pages done

--- src/pages/TransferCoins.js
+++ src/pages/TransferCoins.js
@@ -19,8 +19,13 @@ import {
     Radio,
     RadioGroup,
     IconButton,
-    useColorMode,
     Text,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
 } from '@chakra-ui/react';
 
 import Container from '../components/Container'
@@ -31,11 +36,30 @@ import { Step, Steps, useSteps } from "chakra-ui-steps"
 
 const steps = [{ label: "Add Recipients" }, { label: "Verify" }]
 
-function RecipientsForm() {
+function Content({ index }) {
+}
+
+export default function TransferCoins() {
+    useEffect(() => {
+        document.title = "Transfer Coins | LastWord";
+        const metaDescription = document.createElement('meta');
+        metaDescription.name = 'description';
+        metaDescription.content = 'Your voice beyond life.';
+        document.head.appendChild(metaDescription);
+
+    }, []);
+    const { nextStep, prevStep, reset, activeStep } = useSteps({
+        initialStep: 0,
+    })
+
     const [pensionOption, setPensionOption] = useState("once");
     const [recipients, setRecipients] = useState([
         { walletAddress: "", emailAddress: "", percent: "" }
     ]);
+    useEffect(() => {
+        console.log(recipients)
+
+    }, [recipients]);
 
     const handlePensionOptionChange = (value) => {
         setPensionOption(value);
@@ -65,89 +89,6 @@ function RecipientsForm() {
     };
 
     return (
-        <VStack spacing="12">
-            <FormControl as="fieldset">
-                <FormLabel as="legend"><strong>Payment option</strong></FormLabel>
-                <RadioGroup defaultValue={pensionOption} onChange={handlePensionOptionChange}>
-                    <HStack spacing="6">
-                        <Radio value="once">Pay at once</Radio>
-                        <Radio value="pension">Pay like a pension</Radio>
-                    </HStack>
-                </RadioGroup>
-            </FormControl>
-
-
-            <VStack>
-                <HStack>
-                    <FormControl w='22vw' ml='-2vw'><FormLabel>Wallet Address</FormLabel></FormControl>
-                    <FormControl w='15vw'><FormLabel>Email Address</FormLabel></FormControl>
-                    <FormControl w='5vw'><FormLabel>% Share</FormLabel></FormControl>
-                </HStack>
-                {recipients.map((recipient, index) => (
-                    <VStack key={index} spacing="4">
-                        <HStack spacing="4">
-                            <FormControl>
-                                <Input
-                                    value={recipient.walletAddress}
-                                    onChange={(event) => handleRecipientChange(index, "walletAddress", event.target.value)}
-                                    placeholder="0x1234..."
-                                    w='22vw'
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <Input
-                                    value={recipient.emailAddress}
-                                    onChange={(event) => handleRecipientChange(index, "emailAddress", event.target.value)}
-                                    placeholder="example@example.com"
-                                    w='15vw'
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <InputGroup w='5vw'>
-                                    <NumberInput value={recipient.percentage} max={100 / recipients.length} min={0} onChange={(valueString) => handlePercentChange(index, valueString)}>
-                                        <NumberInputField />
-                                        <NumberInputStepper>
-                                            <NumberIncrementStepper />
-                                            <NumberDecrementStepper />
-                                        </NumberInputStepper>
-                                    </NumberInput>
-                                </InputGroup>
-                            </FormControl>
-                            <IconButton icon={<DeleteIcon />} variant="ghost" onClick={() => handleRemoveRecipient(index)} />
-                        </HStack>
-                    </VStack>
-                ))}
-            </VStack>
-            <Button leftIcon={<AddIcon />} onClick={handleAddRecipient}>Add recipient</Button>
-        </VStack >
-    );
-}
-
-function Content({ index }) {
-    if (index === 0) {
-        return <RecipientsForm />;
-    } else if (index === 1) {
-        return <Box w="200px" h="200px" bg="red" />;
-    } else {
-        return null;
-    }
-}
-
-export default function TransferCoins() {
-    useEffect(() => {
-        document.title = "Transfer Coins | LastWord";
-        const metaDescription = document.createElement('meta');
-        metaDescription.name = 'description';
-        metaDescription.content = 'Your voice beyond life.';
-        document.head.appendChild(metaDescription);
-
-    }, []);
-    const { nextStep, prevStep, reset, activeStep } = useSteps({
-        initialStep: 0,
-    })
-    const { colorMode } = useColorMode()
-
-    return (
         <Container>
             <Stack
                 spacing={{ base: 8, md: 10 }}
@@ -161,7 +102,82 @@ export default function TransferCoins() {
                         <Steps activeStep={activeStep}>
                             {steps.map(({ label }, index) => (
                                 <Step label={label} key={label}>
-                                    <Content index={index} />
+                                    {index === 0 ?
+                                        (
+                                            <VStack spacing="12">
+                                                <FormControl as="fieldset">
+                                                    <FormLabel as="legend"><strong>Payment option</strong></FormLabel>
+                                                    <RadioGroup defaultValue={pensionOption} onChange={handlePensionOptionChange}>
+                                                        <HStack spacing="6">
+                                                            <Radio value="once">Pay at once</Radio>
+                                                            <Radio value="pension">Pay like a pension</Radio>
+                                                        </HStack>
+                                                    </RadioGroup>
+                                                </FormControl>
+                                                <VStack>
+                                                    <HStack>
+                                                        <FormControl w='22vw' ml='-2vw'><FormLabel><strong>Wallet Address</strong></FormLabel></FormControl>
+                                                        <FormControl w='15vw'><FormLabel><strong>Email Address</strong></FormLabel></FormControl>
+                                                        <FormControl w='5vw'><FormLabel><strong>% Share</strong></FormLabel></FormControl>
+                                                    </HStack>
+                                                    {recipients.map((recipient, index) => (
+                                                        <VStack key={index} spacing="4">
+                                                            <HStack spacing="4">
+                                                                <FormControl>
+                                                                    <Input
+                                                                        value={recipient.walletAddress}
+                                                                        onChange={(event) => handleRecipientChange(index, "walletAddress", event.target.value)}
+                                                                        placeholder="0x1234..."
+                                                                        w='22vw'
+                                                                    />
+                                                                </FormControl>
+                                                                <FormControl>
+                                                                    <Input
+                                                                        value={recipient.emailAddress}
+                                                                        onChange={(event) => handleRecipientChange(index, "emailAddress", event.target.value)}
+                                                                        placeholder="example@example.com"
+                                                                        w='15vw'
+                                                                    />
+                                                                </FormControl>
+                                                                <FormControl>
+                                                                    <InputGroup w='5vw'>
+                                                                        <NumberInput value={recipient.percentage} max={100} min={0} onChange={(valueString) => handlePercentChange(index, valueString)}>
+                                                                            <NumberInputField />
+                                                                            <NumberInputStepper>
+                                                                                <NumberIncrementStepper />
+                                                                                <NumberDecrementStepper />
+                                                                            </NumberInputStepper>
+                                                                        </NumberInput>
+                                                                    </InputGroup>
+                                                                </FormControl>
+                                                                <IconButton icon={<DeleteIcon />} variant="ghost" onClick={() => handleRemoveRecipient(index)} />
+                                                            </HStack>
+                                                        </VStack>
+                                                    ))}
+                                                </VStack>
+                                                <Button leftIcon={<AddIcon />} onClick={handleAddRecipient}>Add recipient</Button>
+                                            </VStack >
+                                        ) :
+                                        (
+                                            <Table>
+                                                <Thead>
+                                                    <Tr>
+                                                        <Th>Wallet Address</Th>
+                                                        <Th>Email Address</Th>
+                                                        <Th>Percentage</Th>
+                                                    </Tr>
+                                                </Thead>
+                                                <Tbody>
+                                                    {recipients.map((recipient, index) => (
+                                                        <Tr key={index}>
+                                                            <Td>{recipient.walletAddress}</Td>
+                                                            <Td>{recipient.emailAddress}</Td>
+                                                            <Td>{recipient.percentage}</Td>
+                                                        </Tr>
+                                                    ))}
+                                                </Tbody>
+                                            </Table>
+                                        )}
                                 </Step>
                             ))}
                         </Steps>
