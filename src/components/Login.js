85a514975975be5bb69aa7191a15c1a705ecac95
madhav2391 <psvsaimadhav@gmail.com>
2023-02-19 11:59:38 +0530

    arcana auth

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,15 +6,29 @@ import {
     MenuList,
     MenuItem,
     Link,
-    useColorMode
+    useColorMode,
+    Spinner
 } from '@chakra-ui/react';
+import { Auth, useAuth } from "@arcana/auth-react";
+import { redirect } from "react-router-dom";
 
+const onLogin = () => {
+    // Route to authenticated page
+    redirect("/dashboard");
+  }
 const Login = () => {
 
     const { colorMode } = useColorMode();
-
+    const auth = useAuth();
     return (
-        <Menu>
+        <div>
+          {auth.loading ? (
+            <Spinner />
+          ) : auth.isLoggedIn ? (
+            <p>auth.user</p>
+          ) : (
+            <div>
+                       <Menu>
             <MenuButton
                 as={Button}
                 display={{ base: 'none', md: 'inline-flex' }}
@@ -24,8 +38,31 @@ const Login = () => {
             >
                 Connect Wallet
             </MenuButton>
+            <MenuList>
+            <Auth externalWallet={true} theme={"light"} onLogin={onLogin}/>
+
+            </MenuList>
         </Menu>
-    )
+
+            </div>
+          )}
+        </div>
+      );
+
+
+    // return (
+    //     <Menu>
+    //         <MenuButton
+    //             as={Button}
+    //             display={{ base: 'none', md: 'inline-flex' }}
+    //             fontSize={'sm'}
+    //             fontWeight={600}
+    //             href={'#'}
+    //         >
+    //             Connect Wallet
+    //         </MenuButton>
+    //     </Menu>
+    // )
 }
 
 export default Login;
